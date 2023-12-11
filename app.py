eed76eb6575628353da1ecb175e10b38076b10e9
Finley <trinity.tringl@gmail.com>
2023-12-11 12:07:20 +1000

    table classes added

--- app.py
+++ app.py
@@ -1,13 +1,67 @@
-from flask import Flask
+from flask import Flask, request, jsonify, session
 from flask_sqlalchemy import SQLAlchemy
-from datetime import date
-import psycopg2
+from datetime import datetime
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
-url = os.getenv('DATABASE_URL')
-connection = psycopg2.connect(url)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(80), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    notes = db.Column(db.String(200))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class Income(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float, nullable=False)
+    source = db.Column(db.String(80), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class Budget(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(80), nullable=False)
+    time_frame = db.Column(db.String(80), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+@app.route('/register', methods=['POST'])
+def register():
+    pass
+
+@app.route('/login', methods=['POST'])
+def login():
+    pass
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    pass
+
+@app.route('/expenses', methods=['POST', 'GET', 'PUT', 'DELETE'])
+def manage_expenses():
+    pass
+
+@app.route('/incomes', methods=['POST', 'GET', 'PUT', 'DELETE'])
+def manage_incomes():
+    pass
+
+@app.route('/budgets', methods=['POST', 'GET', 'PUT', 'DELETE'])
+def manage_budgets():
+    pass
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
