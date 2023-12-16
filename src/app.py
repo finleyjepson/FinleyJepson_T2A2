187959a1c421ef5159a90d338d897872aaa679ea
Finley <trinity.tringl@gmail.com>
2023-12-17 08:28:39 +1000

    Fix bug in login functionality
    Add validation for user input
    
    Refactor code for better readability
    
    Update error handling in API calls
    
    Optimize database queries for improved performance

--- src/app.py
+++ src/app.py
@@ -8,121 +8,145 @@ from sqlalchemy import extract
 
 app = Blueprint('app', __name__)
 
-@app.route('/expense', methods=['POST', 'GET', 'PUT', 'DELETE'])
+
+@app.route('/expense', methods=['POST'])
 @jwt_required()
-def manage_expenses():
+def create_expense():
     current_user = get_jwt_identity()
     if not current_user:
         return jsonify({'message': 'Unauthorized'}), 401
-    match request.method:
-        case 'POST':
-            # Handle sending expenses
-            amount = request.args.get('amount')
-            category = request.args.get('category')
-            new_expense = Expense(
-                userid=current_user,
-                amount=amount,
-                category=category
-            )
-            db.session.add(new_expense)
-            db.session.commit()
-            return jsonify({'message': 'Expense sent successfully'}), 200
-        case 'DELETE':
-            # Handle deleting expenses
-            expense_id = request.args.get('expense_id')
-            expense = Expense.query.filter_by(expenseid=expense_id, userid=current_user).first()
-            if expense:
-                db.session.delete(expense)
-                db.session.commit()
-                return jsonify({'message': 'Expense deleted successfully'}), 200
-            else:
-                return jsonify({'message': 'Expense not found'}), 404
-        case 'PUT':
-            # Handle editing expenses
-            expense_id = request.args.get('expense_id')
-            expense = Expense.query.filter_by(expenseid=expense_id, userid=current_user).first()
-            if expense:
-                expense_data = request.args
-                expense.amount = expense_data.get('amount')
-                expense.category = expense_data.get('category')
-                db.session.commit()
-                return jsonify({'message': 'Income updated successfully'}), 200
-            else:
-                return jsonify({'message': 'Income not found'}), 404
-        case 'GET':
-            # Handle getting expenses
-            expense_id = request.args.get('expense_id')
-            if expense_id:
-                expenses = Expense.query.filter_by(userid=current_user, expenseid=expense_id).all()
-            else:
-                expenses = Expense.query.filter_by(userid=current_user).all()
-            expense_list = []
-            for expense in expenses:
-                expense_data = {
-                    'expenseid': expense.expenseid,
-                    'amount': expense.amount,
-                    'category': expense.category,
-                    'date': expense.date
-                }
-                expense_list.append(expense_data)
-            return jsonify(expense_list), 200
-        case _:
-            return jsonify({'message': 'Method not allowed'}), 405
 
-@app.route('/income', methods=['POST', 'GET', 'PUT', 'DELETE'])
+    amount = request.args.get('amount')
+    category = request.args.get('category')
+    new_expense = Expense(
+        userid=current_user,
+        amount=amount,
+        category=category
+    )
+    db.session.add(new_expense)
+    db.session.commit()
+    return jsonify({'message': 'Expense sent successfully'}), 200
+
+
+@app.route('/expense', methods=['DELETE'])
 @jwt_required()
-def manage_incomes():
+def delete_expense():
     current_user = get_jwt_identity()
-    match request.method:
-        case 'POST':
-            # Handle sending income
-            amount = request.args.get('amount')
-            source = request.args.get('source')
-            new_income = Income(
-                userid=current_user,
-                amount=amount,
-                source=source
-            )
-            db.session.add(new_income)
-            db.session.commit()
-            return jsonify({'message': 'Income sent successfully'}), 200
-        case 'DELETE':
-            # Handle deleting income
-            income_id = request.args.get('income_id')
-            income = Income.query.filter_by(incomeid=income_id, userid=current_user).first()
-            if income:
-                db.session.delete(income)
-                db.session.commit()
-                return jsonify({'message': 'Income deleted successfully'}), 200
-            else:
-                return jsonify({'message': 'Income not found'}), 404
-        case 'PUT':
-            # Handle editing income
-            income_id = request.args.get('income_id')
-            income = Income.query.filter_by(incomeid=income_id, userid=current_user).first()
-            if income:
-                income_data = request.args
-                income.amount = income_data.get('amount')
-                income.source = income_data.get('source')
-                db.session.commit()
-                return jsonify({'message': 'Income updated successfully'}), 200
-            else:
-                return jsonify({'message': 'Income not found'}), 404
-        case 'GET':
-            # Handle getting income
-            incomes = Income.query.filter_by(userid=current_user).all()
-            income_list = []
-            for income in incomes:
-                income_data = {
-                    'incomeid': income.incomeid,
-                    'amount': income.amount,
-                    'source': income.source,
-                    'date': income.date
-                }
-                income_list.append(income_data)
-            return jsonify(income_list), 200
-        case _:
-            return jsonify({'message': 'Method not allowed'}), 405
+    if not current_user:
+        return jsonify({'message': 'Unauthorized'}), 401
+
+    expense_id = request.args.get('expense_id')
+    expense = Expense.query.filter_by(expenseid=expense_id, userid=current_user).first()
+    if expense:
+        db.session.delete(expense)
+        db.session.commit()
+        return jsonify({'message': 'Expense deleted successfully'}), 200
+    else:
+        return jsonify({'message': 'Expense not found'}), 404
+
+
+@app.route('/expense', methods=['PUT'])
+@jwt_required()
+def update_expense():
+    current_user = get_jwt_identity()
+    if not current_user:
+        return jsonify({'message': 'Unauthorized'}), 401
+
+    expense_id = request.args.get('expense_id')
+    expense = Expense.query.filter_by(expenseid=expense_id, userid=current_user).first()
+    if expense:
+        expense_data = request.args
+        expense.amount = expense_data.get('amount')
+        expense.category = expense_data.get('category')
+        db.session.commit()
+        return jsonify({'message': 'Expense updated successfully'}), 200
+    else:
+        return jsonify({'message': 'Expense not found'}), 404
+
+
+@app.route('/expense', methods=['GET'])
+@jwt_required()
+def get_expenses():
+    current_user = get_jwt_identity()
+    if not current_user:
+        return jsonify({'message': 'Unauthorized'}), 401
+
+    expense_id = request.args.get('expense_id')
+    if expense_id:
+        expenses = Expense.query.filter_by(userid=current_user, expenseid=expense_id).all()
+    else:
+        expenses = Expense.query.filter_by(userid=current_user).all()
+    expense_list = []
+    for expense in expenses:
+        expense_data = {
+            'expenseid': expense.expenseid,
+            'amount': expense.amount,
+            'category': expense.category,
+            'date': expense.date
+        }
+        expense_list.append(expense_data)
+    return jsonify(expense_list), 200
+
+
+
+@app.route('/income', methods=['POST'])
+@jwt_required()
+def create_income():
+    current_user = get_jwt_identity()
+    amount = request.args.get('amount')
+    source = request.args.get('source')
+    new_income = Income(
+        userid=current_user,
+        amount=amount,
+        source=source
+    )
+    db.session.add(new_income)
+    db.session.commit()
+    return jsonify({'message': 'Income sent successfully'}), 200
+
+@app.route('/income', methods=['GET'])
+@jwt_required()
+def get_incomes():
+    current_user = get_jwt_identity()
+    incomes = Income.query.filter_by(userid=current_user).all()
+    income_list = []
+    for income in incomes:
+        income_data = {
+            'incomeid': income.incomeid,
+            'amount': income.amount,
+            'source': income.source,
+            'date': income.date
+        }
+        income_list.append(income_data)
+    return jsonify(income_list), 200
+
+@app.route('/income', methods=['PUT'])
+@jwt_required()
+def update_income():
+    current_user = get_jwt_identity()
+    income_id = request.args.get('income_id')
+    income = Income.query.filter_by(incomeid=income_id, userid=current_user).first()
+    if income:
+        income_data = request.args
+        income.amount = income_data.get('amount')
+        income.source = income_data.get('source')
+        db.session.commit()
+        return jsonify({'message': 'Income updated successfully'}), 200
+    else:
+        return jsonify({'message': 'Income not found'}), 404
+
+@app.route('/income', methods=['DELETE'])
+@jwt_required()
+def delete_income():
+    current_user = get_jwt_identity()
+    income_id = request.args.get('income_id')
+    income = Income.query.filter_by(incomeid=income_id, userid=current_user).first()
+    if income:
+        db.session.delete(income)
+        db.session.commit()
+        return jsonify({'message': 'Income deleted successfully'}), 200
+    else:
+        return jsonify({'message': 'Income not found'}), 404
 
 @app.route('/budget', methods=['POST'])
 @jwt_required()
@@ -218,10 +242,3 @@ def delete_budget():
         return jsonify({'message': 'Budget deleted successfully'}), 200
     else:
         return jsonify({'message': 'Budget not found'}), 404
-@app.errorhandler(404)
-def not_found(error):
-    return jsonify({'message': 'Not found'}), 404
-
-@app.errorhandler(500)
-def internal_server_error(error):
-    return jsonify({'message': 'Internal server error'}), 500
