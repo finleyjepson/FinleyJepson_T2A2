0ebdbb33781b82b2ebd5b32221cc657430804feb
Finley <trinity.tringl@gmail.com>
2023-12-17 10:10:44 +1000

    Refactor code to improve performance and readability

--- src/app.py
+++ src/app.py
@@ -1,159 +1,155 @@
-from flask import jsonify, session, Blueprint
-from . import db
-from flask import request
-from .models import Income, Expense, Budget
-from datetime import date
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from functools import wraps 
+from flask import jsonify, session, Blueprint 
+from . import db 
+from flask import request 
+from .models import Income, Expense, Budget 
+from datetime import date 
+from flask_jwt_extended import jwt_required, get_jwt_identity 
 from sqlalchemy import extract
 
 app = Blueprint('app', __name__)
 
+# Decorator to require JWT authentication and user identity
+def jwt_required_and_user(f): 
+    @wraps(f) 
+    @jwt_required()
+    def decorated(*args, **kwargs): 
+        current_user = get_jwt_identity() 
+        if not current_user: 
+            return jsonify({'message': 'Unauthorized'}), 401 
+        return f(*args, **kwargs) 
+    return decorated
 
-@app.route('/expense', methods=['POST'])
-@jwt_required()
-def create_expense():
-    current_user = get_jwt_identity()
-    if not current_user:
-        return jsonify({'message': 'Unauthorized'}), 401
+# Helper function to handle not found entities
+def handle_not_found(entity): 
+    return jsonify({'message': f'{entity} not found'}), 404
 
-    amount = request.args.get('amount')
-    category = request.args.get('category')
-    new_expense = Expense(
-        userid=current_user,
-        amount=amount,
-        category=category
-    )
-    db.session.add(new_expense)
+# Helper function to create a new entity
+def create_entity(entity_class, **kwargs): 
+    new_entity = entity_class(**kwargs) 
+    db.session.add(new_entity) 
     db.session.commit()
+    return new_entity
+
+# Route to create an expense
+@app.route('/expense', methods=['POST']) 
+@jwt_required_and_user 
+def create_expense(): 
+    current_user = get_jwt_identity() 
+    amount = request.args.get('amount') 
+    category = request.args.get('category') 
+    create_entity(Expense, userid=current_user, amount=amount, category=category) 
     return jsonify({'message': 'Expense sent successfully'}), 200
 
-
-@app.route('/expense', methods=['DELETE'])
-@jwt_required()
-def delete_expense():
-    current_user = get_jwt_identity()
-    if not current_user:
-        return jsonify({'message': 'Unauthorized'}), 401
-
-    expense_id = request.args.get('expense_id')
-    expense = Expense.query.filter_by(expenseid=expense_id, userid=current_user).first()
-    if expense:
+# Route to delete an expense
+@app.route('/expense', methods=['DELETE']) 
+@jwt_required_and_user 
+def delete_expense(): 
+    current_user = get_jwt_identity() 
+    expense_id = request.args.get('expense_id') 
+    expense = Expense.query.filter_by(expenseid=expense_id, userid=current_user).first() 
+    if expense: 
         db.session.delete(expense)
-        db.session.commit()
-        return jsonify({'message': 'Expense deleted successfully'}), 200
-    else:
-        return jsonify({'message': 'Expense not found'}), 404
+        db.session.commit() 
+        return jsonify({'message': 'Expense deleted successfully'}), 200 
+    else: 
+        return handle_not_found('Expense')
 
+# Route to update an expense
+@app.route('/expense', methods=['PUT']) 
+@jwt_required_and_user 
+def update_expense(): 
+    current_user = get_jwt_identity() 
+    expense_id = request.args.get('expense_id') 
+    expense = Expense.query.filter_by(expenseid=expense_id, userid=current_user).first() 
+    if expense: 
+        expense_data = request.args 
+        expense.amount = expense_data.get('amount') 
+        expense.category = expense_data.get('category') 
+        db.session.commit() 
+        return jsonify({'message': 'Expense updated successfully'}), 200 
+    else: 
+        return handle_not_found('Expense')
 
-@app.route('/expense', methods=['PUT'])
-@jwt_required()
-def update_expense():
-    current_user = get_jwt_identity()
-    if not current_user:
-        return jsonify({'message': 'Unauthorized'}), 401
-
-    expense_id = request.args.get('expense_id')
-    expense = Expense.query.filter_by(expenseid=expense_id, userid=current_user).first()
-    if expense:
-        expense_data = request.args
-        expense.amount = expense_data.get('amount')
-        expense.category = expense_data.get('category')
-        db.session.commit()
-        return jsonify({'message': 'Expense updated successfully'}), 200
-    else:
-        return jsonify({'message': 'Expense not found'}), 404
-
-
-@app.route('/expense', methods=['GET'])
-@jwt_required()
-def get_expenses():
-    current_user = get_jwt_identity()
-    if not current_user:
-        return jsonify({'message': 'Unauthorized'}), 401
-
-    expense_id = request.args.get('expense_id')
-    if expense_id:
-        expenses = Expense.query.filter_by(userid=current_user, expenseid=expense_id).all()
-    else:
-        expenses = Expense.query.filter_by(userid=current_user).all()
-    expense_list = []
-    for expense in expenses:
-        expense_data = {
-            'expenseid': expense.expenseid,
-            'amount': expense.amount,
-            'category': expense.category,
-            'date': expense.date
-        }
-        expense_list.append(expense_data)
+# Route to get expenses
+@app.route('/expense', methods=['GET']) 
+@jwt_required_and_user 
+def get_expenses(): 
+    # Get the current user's identity from the JWT token
+    current_user = get_jwt_identity() 
+    # Get the expense ID from the request parameters
+    expense_id = request.args.get('expense_id') 
+    # Check if an expense ID is provided
+    if expense_id: 
+        expenses = Expense.query.filter_by(userid=current_user, expenseid=expense_id).all() 
+    else: 
+        expenses = Expense.query.filter_by(userid=current_user).all() 
+    expense_list = [] 
+    # Convert the expenses to a list of dictionaries
+    for expense in expenses: 
+        expense_data = { 'expenseid': expense.expenseid, 'amount': expense.amount, 'category': expense.category, 'date': expense.date } 
+        expense_list.append(expense_data) 
     return jsonify(expense_list), 200
 
-
-
-@app.route('/income', methods=['POST'])
-@jwt_required()
-def create_income():
-    current_user = get_jwt_identity()
-    amount = request.args.get('amount')
-    source = request.args.get('source')
-    new_income = Income(
-        userid=current_user,
-        amount=amount,
-        source=source
-    )
-    db.session.add(new_income)
-    db.session.commit()
+# Route to create an income
+@app.route('/income', methods=['POST']) 
+@jwt_required_and_user 
+def create_income(): 
+    current_user = get_jwt_identity() 
+    amount = request.args.get('amount') 
+    source = request.args.get('source') 
+    create_entity(Income, userid=current_user, amount=amount, source=source) 
     return jsonify({'message': 'Income sent successfully'}), 200
 
-@app.route('/income', methods=['GET'])
-@jwt_required()
-def get_incomes():
-    current_user = get_jwt_identity()
-    incomes = Income.query.filter_by(userid=current_user).all()
-    income_list = []
-    for income in incomes:
-        income_data = {
-            'incomeid': income.incomeid,
-            'amount': income.amount,
-            'source': income.source,
-            'date': income.date
-        }
-        income_list.append(income_data)
+# Route to get incomes
+@app.route('/income', methods=['GET']) 
+@jwt_required_and_user 
+def get_incomes(): 
+    current_user = get_jwt_identity() 
+    incomes = Income.query.filter_by(userid=current_user).all() 
+    income_list = [] 
+    for income in incomes: 
+        income_data = { 'incomeid': income.incomeid, 'amount': income.amount, 'source': income.source, 'date': income.date } 
+        income_list.append(income_data) 
     return jsonify(income_list), 200
 
-@app.route('/income', methods=['PUT'])
-@jwt_required()
-def update_income():
-    current_user = get_jwt_identity()
-    income_id = request.args.get('income_id')
-    income = Income.query.filter_by(incomeid=income_id, userid=current_user).first()
-    if income:
-        income_data = request.args
-        income.amount = income_data.get('amount')
-        income.source = income_data.get('source')
-        db.session.commit()
-        return jsonify({'message': 'Income updated successfully'}), 200
-    else:
-        return jsonify({'message': 'Income not found'}), 404
+# Route to update an income
+@app.route('/income', methods=['PUT']) 
+@jwt_required_and_user 
+def update_income(): 
+    current_user = get_jwt_identity() 
+    income_id = request.args.get('income_id') 
+    income = Income.query.filter_by(incomeid=income_id, userid=current_user).first() 
+    if income: 
+        income_data = request.args 
+        income.amount = income_data.get('amount') 
+        income.source = income_data.get('source') 
+        db.session.commit() 
+        return jsonify({'message': 'Income updated successfully'}), 200 
+    else: 
+        return handle_not_found('Income')
 
-@app.route('/income', methods=['DELETE'])
-@jwt_required()
-def delete_income():
-    current_user = get_jwt_identity()
-    income_id = request.args.get('income_id')
-    income = Income.query.filter_by(incomeid=income_id, userid=current_user).first()
+# Route to delete an income
+@app.route('/income', methods=['DELETE']) 
+@jwt_required_and_user 
+def delete_income(): 
+    current_user = get_jwt_identity() 
+    income_id = request.args.get('income_id') 
+    income = Income.query.filter_by(incomeid=income_id, userid=current_user).first() 
     if income:
-        db.session.delete(income)
-        db.session.commit()
-        return jsonify({'message': 'Income deleted successfully'}), 200
+        db.session.delete(income) 
+        db.session.commit() 
+        return jsonify({'message': 'Income deleted successfully'}), 200 
     else:
-        return jsonify({'message': 'Income not found'}), 404
+        return handle_not_found('Income')
 
-@app.route('/budget', methods=['POST'])
-@jwt_required()
-def create_budget():
-    current_user = get_jwt_identity()
+# Route to create a budget
+@app.route('/budget', methods=['POST']) 
+@jwt_required_and_user 
+def create_budget(): 
+    current_user = get_jwt_identity() 
     current_year, current_month = date.now().year, date.now().month
-
+    
     # Calculate total income for the current month
     incomes = Income.query.filter(
         Income.userid == current_user, 
@@ -163,7 +159,7 @@ def create_budget():
     total_income = 0
     for income in incomes:
         total_income += income.amount
-
+    
     # Calculate total expense for the current month
     expenses = Expense.query.filter(
         Expense.userid == current_user, 
@@ -173,65 +169,47 @@ def create_budget():
     total_expense = 0
     for expense in expenses:
         total_expense += expense.amount
-
+    
     # Calculate total budget for the current month
     total_budget = total_income - total_expense
+    
+    # Create a new budget entity
+    create_entity(Budget, userid=current_user, total_budget=total_budget, time_frame=str(current_year)+'-'+str(current_month))
+    
+    return jsonify({'message': 'Budget created successfully'}), 201
 
-    # Create a new budget entry in the database
-    new_budget = Budget(userid=current_user, total_budget=total_budget, time_frame=str(current_year)+'-'+str(current_month))
-    db.session.add(new_budget)
-    db.session.commit()
-
-    return jsonify(new_budget.to_dict()), 201
-
-@app.route('/budget', methods=['GET'])
-@jwt_required()
+# Route to get budgets
+@app.route('/budget', methods=['GET']) 
+@jwt_required_and_user 
 def get_budget():
-    # Get the budgetid from the request arguments
     budget_id = request.args.get('budget_id')
-    # Get the userid from the JWT token
     current_user = get_jwt_identity()
-
     if budget_id:
-        # If budgetid is provided, filter budgets by userid and budgetid
         budgets = Budget.query.filter_by(userid=current_user, budgetid=budget_id).all()
     else:
-        # If budgetid is not provided, filter budgets by userid only
         budgets = Budget.query.filter_by(userid=current_user).all()
-
-    # Serialize the budgets into a JSON response
     return jsonify([budget.serialize() for budget in budgets]), 200
 
+# Route to update a budget
 @app.route('/budget', methods=['PUT'])
-@jwt_required()
+@jwt_required_and_user
 def update_budget():
     current_user = get_jwt_identity()
-
-    # Get the budget_id from the request arguments
     budget_id = request.args.get('budget_id')
-
-    # Find the budget with the given budget_id and current_user
     budget = Budget.query.filter_by(budgetid=budget_id, userid=current_user).first()
-
     if budget:
-        # Get the budget data from the request arguments
         budget_data = request.args
-
-        # Update the budget amount and time frame
         budget.amount = budget_data.get('amount')
         budget.time_frame = budget_data.get('time_frame')
         budget.last_modified_date = date.now()
-        # Commit the changes to the database
         db.session.commit()
-
-        # Return a success message
         return jsonify({'message': 'Budget updated successfully'}), 200
     else:
-        # Return an error message if the budget is not found
-        return jsonify({'message': 'Budget not found'}), 404
+        return handle_not_found('Budget')
 
+# Route to delete a budget
 @app.route('/budget', methods=['DELETE'])
-@jwt_required()
+@jwt_required_and_user
 def delete_budget():
     current_user = get_jwt_identity()
     budget_id = request.args.get('budget_id')
@@ -241,4 +219,4 @@ def delete_budget():
         db.session.commit()
         return jsonify({'message': 'Budget deleted successfully'}), 200
     else:
-        return jsonify({'message': 'Budget not found'}), 404
+        return handle_not_found('Budget')
